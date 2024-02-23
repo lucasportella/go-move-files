13047f54aab24791f874346afe727fd96856e296
Lucas Portella <lucasportellaagu@gmail.com>
2024-02-22 21:15:09 -0300

    refactor: main try to organize moveFilesDefault fn a little

--- main.go
+++ main.go
@@ -42,47 +42,39 @@ func WalkThroughPaths(paths types.Paths) {
 }
 
 func moveFilesDefault(key string, innerPaths types.InnerPaths) {
+	// get paths from json
 	srcPath := innerPaths.Src_dir
 	dstPath := innerPaths.Dst_dir
-	src, err := os.Open(srcPath)
 
-	if err != nil {
-		log.Fatalf("Fatal! Could not open source directory. Error: %v\n", err)
-	}
-	defer src.Close()
-
-	files, err := src.ReadDir(-1)
-
+	// read files from src dir
+	files, err := os.ReadDir(srcPath)
 	if err != nil {
 		log.Fatalf("Fatal! Could not read source directory. Error: %v\n", err)
 	}
 
+	//loop through src dir files
 	for _, file := range files {
 
+		//openFile in the src dir
 		oldFilePath := srcPath + "/" + file.Name()
 		srcFile, err := openFile(oldFilePath)
 		if err != nil {
 			log.Printf("Error while opening the file using the old path. Path: %v", oldFilePath)
-		} else {
-			defer srcFile.Close()
 		}
+		defer srcFile.Close()
 
+		// create file in dst dir if file matches the key
 		newFilePath := dstPath + "/" + file.Name()
 		if strings.HasPrefix(file.Name(), key) {
 			dstFile, err := os.Create(newFilePath)
 			if err != nil {
 				log.Printf("Error while creating file in destiny folder: %v\n", err)
+				dstFile.Close()
 				continue
 			}
+			defer dstFile.Close()
+			moveFile(dstFile, srcFile, newFilePath, oldFilePath)
 
-			_, err = io.Copy(dstFile, srcFile)
-			srcFile.Close()
-			if err != nil {
-				fmt.Printf("Error while moving file: %v\n", err)
-				deleteFile(newFilePath)
-			} else {
-				deleteFile(oldFilePath)
-			}
 		}
 	}
 }
@@ -104,4 +96,16 @@ func openFile(filePath string) (*os.File, error) {
 		return nil, err
 	}
 	return file, nil
+}
+
+func moveFile(dstFile *os.File, srcFile *os.File, newFilePath string, oldFilePath string) {
+	// copy the file from src to dst and delete src file if success or delete dst file if fails
+	_, err := io.Copy(dstFile, srcFile)
+	srcFile.Close()
+	if err != nil {
+		fmt.Printf("Error while moving file: %v\n", err)
+		deleteFile(newFilePath)
+	} else {
+		deleteFile(oldFilePath)
+	}
 }
