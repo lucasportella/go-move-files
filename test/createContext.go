f5248b7688b669d6d2c0bdac6af09460cb6e69e7
Lucas Portella <lucasportellaagu@gmail.com>
2024-03-04 18:30:33 -0300

    test: setup add files and its content's in setup fn

--- test/createContext.go
+++ test/createContext.go
@@ -18,10 +18,38 @@ func Setup() (string, error) {
 		filepath.Join(projectRoot, "testTemp", "dst"),
 	}
 
+	files := [][]string{
+		{"#bill-1", "content of bill 1"},
+		{"#bill-2", "content of bill 2"},
+		{"#doc-1", "content of doc 1"},
+		{"#doc-2", "content of doc 2"},
+		{"#book-1", "content of book 1"},
+		{"#book-2", "content of book 2"},
+		{"#music-1", "content of music 1"},
+		{"#music-2", "content of music 2"},
+		{"#tax-1", "content of tax 1"},
+		{"#tax-2", "content of tax 2"},
+	}
+
 	for _, dir := range directories {
 		err := os.Mkdir(dir, 0755)
 		if err != nil {
 			return "", fmt.Errorf("error creating directory %s: %w", dir, err)
+		}
+	}
+
+	for _, file := range files {
+		osFile, err := os.Create(filepath.Join(projectRoot, "testTemp", "src", file[0]))
+
+		if err != nil {
+			return "", fmt.Errorf("error creating file %s: %w", file, err)
+		}
+		defer osFile.Close()
+
+		_, err = osFile.WriteString(file[1])
+
+		if err != nil {
+			return "", fmt.Errorf("error while writing content in file %v", file[0])
 		}
 	}
 
